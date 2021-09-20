69ac2bc85eb17539da5689b190588bb2955725e4
Lucas Coelho <lucascoelhodacosta@gmail.com>
2021-09-20 09:16:35 -0300

    fix: prevent outline-me to work inside outline.com

--- chrome/background.js
+++ chrome/background.js
@@ -1,9 +1,13 @@
 const RUN_OUTLINE_ME = 'run_outline_me';
+const OUTLINE_EXP = /https?:\/\/(www\.)?outline.com/g;
 
 async function outlineMe() {
   const queryOptions = { active: true, currentWindow: true };
   const [tab] = await chrome.tabs.query(queryOptions);
   const tabURL = tab.url;
+
+  if (tabURL.match(OUTLINE_EXP)) return;
+
   chrome.tabs.update({ url: `https://outline.com/${tabURL}` });
 }
 
--- firefox/background.js
+++ firefox/background.js
@@ -1,10 +1,14 @@
 const RUN_OUTLINE_ME = 'run_outline_me';
+const OUTLINE_EXP = /https?:\/\/(www\.)?outline.com/g
 
 function outlineMe() {
   browser.tabs.query({active: true, windowId: browser.windows.WINDOW_ID_CURRENT})
     .then(tabs => browser.tabs.get(tabs[0].id))
     .then(tab => {
       const { url } = tab;
+
+      if (url.match(OUTLINE_EXP)) return;
+
       browser.tabs.update({ url: `https://outline.com/${url}` });
   });
 }
